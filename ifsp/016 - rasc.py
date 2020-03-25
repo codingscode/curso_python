47f9687a3d2be744d0d2c72b61c6e3eb87a58452
Heronnes Pereira <heronnestg@hotmail.com>
2020-03-25 16:06:04 -0300

    parte 9

--- ifsp/016 - rasc.py
+++ ifsp/016 - rasc.py
@@ -4,7 +4,7 @@
 
 """
 r - abre arquivo só para leitura
-w - abre arquivo só para editar
+w - abre arquivo só para editar e/ou criar
 a - abre arquivo só acrescentar no final dele
 r+ - abre o arquivo tanto para ler quanto para editar
 
@@ -24,10 +24,43 @@ print(arquivo2.readlines())
 
 print('-----------------')
 
-arquivo3 = open('novo_texto.txt', 'w')  # cria outro arquivo
+arquivo3 = open('novo_texto.txt', 'w')  # cria outro arquivo ou o edita
 arquivo3.write('Testando, testando\n')
 arquivo3.write('mais uma linha')
 arquivo3.close()
+
+print('-----------------')
+
+arquivo4 = open('novo_texto.txt', 'w')    # sobrescreve o arquivo3
+arquivo4.write('Novo texto, no mesmo arquivo')
+arquivo4.close()
+
+print('-----------------')
+
+arquivo5 = open('novo_texto.txt', 'a')    # adiciona ao final do arquivo
+arquivo5.write('\nTexto adicionado ao arquivo com o modo "a"')
+arquivo5.close()
+
+print('-----------------')
+
+# abrindo no modo r+
+
+arquivo6 = open('novo_arquivo.txt', 'r+')
+arquivo6.write('Testando r+')
+#arquivo6.seek(10)
+arquivo6.write('Novo teste do r+')
+#arquivo6.close()
+
+arquivo6.seek(0)
+print(arquivo6.read())
+arquivo6.close()
+
+print('-----------------')
+
+
+
+
+
 
 
 
