a75fc28d71075ddaffdd186b30bad3206d727250
Heronnes Pereira <heronnestg@hotmail.com>
2020-04-09 18:22:41 -0300

    parte 16

---
+++ Work001/file023_mapas.py
@@ -0,0 +1,61 @@
+"""
+Mapas -> conhecidos em python como dicionarios
+representação {}
+
+
+
+"""
+
+receita = {'jan': 100, 'fev': 250, 'marc': 400}
+print(receita)
+
+# Interar sobre dicionarios
+for chave in receita:
+    print(f'{chave} - {receita[chave]}')
+
+print('-------\n')
+
+print(receita.keys())   # mostra as chaves
+
+for chave in receita.keys():  # modo pythonico
+    print(receita[chave])
+
+print('-------\n')
+
+# acessando valores
+print(receita.values())
+
+for valor in receita.values():
+    print(valor)
+
+print('-------\n')
+
+# desempacotamento de dicionarios
+print(receita.items())
+
+for chave, valor in receita.items():
+    print(f'chave: {chave}, valor: {valor}')
+
+print('-------\n')
+
+# soma, maximo, minimo e tamanho
+
+# *Se os valores forem todos inteiros ou reais
+
+print(f'soma: {sum(receita.values())}')
+print(f'maximo: {max(receita.values())}')
+print(f'minimo: {min(receita.values())}')
+print(f'tamanho: {len(receita)}')
+
+
+
+
+
+
+
+
+
+
+
+
+
--- curso_clm/pasta001/00.py
+++ curso_clm/pasta001/00.py
@@ -19,3 +19,66 @@ def, for, lambda, return
 """
 
 # operações : +, -, *, /, **, //, %
+
+oito_por_3 = 8//3
+
+print(oito_por_3)
+print('-----------\n')
+
+uma_string2 = 'goiabada de marmelo'
+
+print(f'{uma_string2} || {uma_string[2]}  || {uma_string2[3:]}')
+
+# concatenando 2 strings
+texto1 = 'antes separados, '
+texto2 = 'e agora juntos'
+textos1_2 = texto1 + texto2
+
+print(f'como fica agora: {textos1_2}\n')
+
+texto4 = 'snake '
+print(f'3 vezes: {texto4*3}\n')
+
+#colocando uma texto em maiusculo e minusculo
+para_maiusculo = 'em maiusculo'
+para_minusculo = 'EM MINUSCULO'
+primeira_maiuscula = 'primeira letra em maiusculo'
+
+print(para_maiusculo.upper())
+print(para_maiusculo.lower())
+print(primeira_maiuscula.capitalize())
+print('----------------\n')
+
+# comparação
+el_1 = 1
+el_2 = '1'
+true_1 = True
+true_2 = 'True'
+
+print(f'el_1 é igual a el_2: {el_1 == el_2}')
+print(f'true_1 é igual a true_2: {true_1 == true_2}\n')
+
+print(f'{1 > 2} || {3 != 2} || {2 == 2}\n')
+
+# usando bool(x), retorna se x é booleano
+
+print(f'{bool(0)} || {bool("")} || {bool(None)} ')
+print(f'{bool(1)} || {bool(-100)} || {bool(13.5)}')
+print(f'{bool("teste")} || {bool(True)}\n')
+
+# outros operadores booleanos
+valor_1 = 5
+valor_2 = 3
+lista_1 = [2, 7, -2, 15, 23]
+elemento = 7
+
+print(f'{valor_1 is valor_2} || {valor_1 is not valor_2} || {elemento in lista_1} || {elemento not in lista_1}\n')
+
+# == e is não são totalmente iguais
+
+lista_2 = [8, 47, 51]
+lista_3 = [8, 47, 51]
+
+print(f'{lista_2 == lista_3} || {lista_2 is lista_3}')
+
+
