672d1202fec74a8b96de61e65ab7b206e0f92029
Heronnes Pereira <heronnestg@hotmail.com>
2020-05-05 21:46:40 -0300

    parte 28

--- Work001/file033_funcoes_c_parametros.py
+++ Work001/file033_funcoes_c_parametros.py
@@ -40,6 +40,56 @@ def repete(a, b, msg):
 print(repete(2, 3, 'repete '))
 print(repete(1, 2, 'python '))
 
+print('------------------')
+# Nomeando parametros
+
+
+def nometodo(nome, sobrenome):
+    return f'Seu nome é : {nome} {sobrenome}'
+
+
+print(nometodo('Vicente', 'di Paolo'))
+
+# diferença entre parametros e argumentos
+"""
+Parametros são variaveis declaradas na definição de uma função;
+Argumentos são dados passados durante a execução de uma função;
+a ordem dos parametros importa 
+"""
+
+# argumentos nomeados (keyword arguments)
+
+"""
+caso utilizemos nomes dos parametros nos argumentos para informa-los, podemos utilizar 
+qualquer ordem.
+
+
+
+"""
+nome = 'Bob'
+sobrenome = 'Simpson'
+
+print(nometodo(nome='Kyle', sobrenome='Adams'))
+print(nometodo(nome=nome, sobrenome=sobrenome))
+print(nometodo(sobrenome='Smith', nome='Jimmy'))
+
+print('-------------------------------')
+# erro comum na utilização do return
+
+
+def soma_impares(numeros):
+    total = 0
+    for numero in numeros:
+        if numero % 2 != 0:
+            total = total + numero
+            #return total erro
+    return total
+
+
+lista = [1, 2, 3, 4, 5, 6, 7]
+print(soma_impares(lista))
+
+
 
 
 
---
+++ Work001/file034_funcoes_parametros_padrao.py
@@ -0,0 +1,114 @@
+"""
+Funções com parametro Padrão
+
+Funções onde a passagem de parametros seja opcional;
+
+
+"""
+# exemplo 1
+
+print('Codando em Python')
+print()   # sem passar
+print('-------------------')
+
+
+def exponencial(numero, expoente=2):
+    return numero**expoente
+
+
+print(exponencial(4, 3))
+print(exponencial(5))  # já informado antes
+print('-----------------------')
+
+# OBS: Em funções Python, os parametros com valores default(padrão) devem sempre estar ao final da declaração
+
+
+def mostrar_informacao(nome='CodePython', instrutor=False):
+    if nome == 'CodePython' and instrutor:
+        return 'Bem vindo instrutor CodePython'
+    elif nome == 'CodePython':
+        return 'Achava q era o instrutor'
+    return f'olá {nome}'
+
+
+print(mostrar_informacao())
+print(mostrar_informacao(instrutor=True))
+print(mostrar_informacao('Jerry'))
+print('---------------------')
+
+# exemplos
+
+
+def soma(num1, num2):
+    return num1 + num2
+
+
+def mat(num1, num2, fun=soma):
+    return fun(num1, num2)
+
+
+def subtracao(num1, num2):
+    return num1 - num2
+
+
+print(mat(2, 3))
+print(mat(2, 3, subtracao))
+
+print('-------------------\n')
+
+# Evitar problemas e confusões...
+# variaveis globais
+# variaveis locais
+
+instrutor = 'Geek'  # v global
+
+
+def diz_oi():
+    ling = 'Python'  # v local
+    return f'Oi {instrutor}, estudando {ling}'
+
+
+print(diz_oi())
+#print(ling)   # dá erro
+print('--------------')
+
+
+total = 0
+
+
+def incrementa():
+    global total
+    total = total + 1
+    return total
+
+
+print(incrementa())
+print(incrementa())
+print(incrementa())
+print('--------------')
+
+# Podemos ter funções que são declaradas dentro de funções, e também uma forma especial de
+# escopo de variavel
+
+
+def fora():
+    contador = 0
+    def dentro():
+        nonlocal contador
+        contador = contador + 1
+        return contador
+    return dentro()
+
+
+print(fora())
+print(fora())
+print(fora())
+#print(dentro())  # dá erro
+
+
+
+
+
+
+
+
