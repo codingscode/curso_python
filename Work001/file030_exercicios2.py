23c671e856467a09e3c9528518580dc33fe9d92e
Heronnes Pereira <heronnestg@hotmail.com>
2020-05-25 16:03:53 -0300

    parte 42

--- Work001/file030_exercicios2.py
+++ Work001/file030_exercicios2.py
@@ -12,6 +12,27 @@ for cadamatriz in matriz1:
 print(f'quantos valores maiores que 10: {contador}')
 
 
+#02
+#https://kharisecario.wordpress.com/2017/03/25/create-nxn-matrix-in-pythonnumpy/
+
+
+ordem = 5
+matriz = []
+
+for i in range(ordem):
+    matriz.append([])
+for cada in matriz:
+    for j in range(ordem):
+        cada.append(0)
+for c in range(len(matriz)):
+    for l in range(len(matriz[c])):
+        if c == l:
+            matriz[c][l] = 1
+        else:
+            matriz[c][l] = 0
+
+
+print(matriz)
 
 
 
@@ -20,7 +41,24 @@ print(f'quantos valores maiores que 10: {contador}')
 
 
 
-#02
+
+
+#02.1
+
+lista = []  #define empty matrix
+ordem = 5
+linha = []  #Mistake position
+
+for i in range(ordem):  #total row is 3
+    linha = []  #Credits for Hassan Tariq for noticing it missing
+    for j in range(ordem):  #total column is 3
+        if i == j:
+            linha.append(1)  #adding 0 value for each column for this row
+        else:
+            linha.append(0)
+    lista.append(linha)  #add fully defined column into the row
+
+print(lista)
 
 
 
