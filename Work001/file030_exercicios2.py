fe6c9f6cbe8a358ce87734289d123bc651a01833
Heronnes Pereira <heronnestg@hotmail.com>
2020-05-25 17:04:39 -0300

    parte 43

--- Work001/file030_exercicios2.py
+++ Work001/file030_exercicios2.py
@@ -34,6 +34,97 @@ for c in range(len(matriz)):
 
 print(matriz)
 
+#02.1
+
+lista = []
+ordem = 5
+linha = []
+
+for i in range(ordem):
+    linha = []
+    for j in range(ordem):
+        if i == j:
+            linha.append(1)
+        else:
+            linha.append(0)
+    lista.append(linha)
+
+print(lista)
+
+#03
+lista = []
+ordem = 4
+linha = []
+
+for i in range(ordem):
+    linha = []
+    for j in range(ordem):
+        linha.append((i+1)*(j+1))
+    lista.append(linha)
+
+print(lista)
+
+
+#04
+matriz = [[2, 0, 9, 5], [4, 10, 21, 6], [12, 10, 2, 5], [11, 3, 15, 3]]
+ref = matriz[0][0]
+posicao = []
+
+for i_coluna in range(len(matriz)):
+    for i_linha in range(len(matriz[i_coluna])):
+        if matriz[i_coluna][i_linha] > ref:
+            ref = matriz[i_coluna][i_linha]
+            posicao = [i_coluna+1, i_linha+1]
+
+print(f'o maior valor é : {ref}, posição(coluna, linha): {posicao}')
+
+
+#05
+valor = int(input('digite o valor a ser procurado: '))
+
+matriz = [[2, 0, 9, 5, 6], [4, 10, 21, 6, 1], [12, 10, 2, 5, 15], [11, 3, 15, 3, 13], [9, 20, 1, 8, 5]]
+ref = valor
+posicao = []
+contador = 0
+
+for i_coluna in range(len(matriz)):
+    for i_linha in range(len(matriz[i_coluna])):
+        if matriz[i_coluna][i_linha] == ref:
+            contador += 1
+            posicao = [i_coluna+1, i_linha+1]
+
+if contador == 0:
+    posicao = 'valor não encontrado'
+
+print(f'valor procurado : {ref}, posição(coluna, linha): {posicao}')
+print(f'ocorrência: {contador} vezes')
+
+
+
+
+
+
+
+
+
+"""
+
+
+
+#11
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 """
@@ -43,24 +134,5 @@ print(matriz)
 
 
 
-#02.1
 
-lista = []  #define empty matrix
-ordem = 5
-linha = []  #Mistake position
-
-for i in range(ordem):  #total row is 3
-    linha = []  #Credits for Hassan Tariq for noticing it missing
-    for j in range(ordem):  #total column is 3
-        if i == j:
-            linha.append(1)  #adding 0 value for each column for this row
-        else:
-            linha.append(0)
-    lista.append(linha)  #add fully defined column into the row
-
-print(lista)
-
-
-
-
-
+"""
