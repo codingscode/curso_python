6aa65aa788c22785e72e1b4d50aff7b81909a6d2
Heronnes Pereira <heronnestg@hotmail.com>
2020-05-29 22:11:33 -0300

    parte 55

---
+++ Work001/file042_set_comprehension.py
@@ -0,0 +1,26 @@
+"""
+
+
+
+
+
+"""
+
+numeros1 = {2, 8, 3, 5, 12}
+print(numeros1)
+
+numeros2 = {num for num in range(1, 7)}
+print(numeros2)
+
+numeros3 = {cada**3 for cada in range(1, 5)}
+print(numeros3)
+
+numeros4 = {cada: cada**3 for cada in range(1, 5)}
+print(numeros4)
+
+# com strings
+letras = {letra for letra in 'Codando Python'}  # não existe ordenação nem repetição
+print(letras)
+
+
+
