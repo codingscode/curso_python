497ed783a924e13986da88996b8e6ca002c0c533
Heronnes Pereira <heronnestg@hotmail.com>
2020-06-26 16:16:40 -0300

    parte 75

---
+++ Work001/file061_modulo_exportado.py
@@ -0,0 +1,14 @@
+"""
+
+"""
+
+
+def soma(a, b):
+    return a + b
+
+
+print('vem do exportado!')
+
+
+def multiplicacao(a, b):
+    return a*b
---
+++ Work001/file061_modulos_customizados.py
@@ -0,0 +1,19 @@
+"""
+Módulos customizados
+
+
+modulos customizados em python são arquivos python prontos para seren utilizados
+
+"""
+from file061_modulo_exportado import soma as algo, multiplicacao as algo2
+
+num1 = 2
+num2 = 3
+
+print(algo(num1, num2))
+print(algo2(num1, num2))
+
+print('----------------------------------')
+"""
+em um modulo python o ideal é não existir print
+"""
