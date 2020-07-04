4f4f34074de2c36d9fc59bd019313035414b6bf8
Heronnes Pereira <heronnestg@hotmail.com>
2020-07-03 21:04:40 -0300

    parte 83

---
+++ Work001/file068_escrever_em_arquivos.py
@@ -0,0 +1,44 @@
+"""
+Escrever em arquivos
+
+
+OBS: Ao abrir um arquivo para leitura, não podemos realizar a escrita nele. Apenas ler. Da mesma
+forma, se abrirmos um arquivo para escrita, não podemos lê-lo, somente escrever nele.
+
+
+"""
+# escrita - modo 'w' -> writing
+
+with open('file068_texto.txt', 'w') as arquivo1:
+    arquivo1.write('Escrever dados em arquivo é bacana.\n')
+    arquivo1.write('Podemos colocar quantas linhas quisermos.\n')
+    arquivo1.write('Esta é a última linha.')
+
+print('novo arquivo criado.')
+"""
+caso já exista um arquivo com o mesmo nome especificado, este será sobrescrito
+"""
+print('-----------------')
+
+with open('file068_outro.txt', 'w') as arquivo2:
+    arquivo2.write('Python\n'*8)
+
+print('-----------------')
+
+with open('file068_fruta.txt', 'w') as arquivo3:
+    while True:
+        fruta = input('digite a fruta ou digite sair: ')
+        if fruta != 'sair':
+            arquivo3.write(f'{fruta}\n')
+        else:
+            break
+
+print('-----------------')
+print('-----------------')
+print('-----------------')
+
+
+
+
+
+
