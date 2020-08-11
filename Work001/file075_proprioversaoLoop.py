37b0131fe3c465a9f78406cd04d567feb2e5ddab
Heronnes Pereira <heronnestg@hotmail.com>
2020-08-11 19:54:20 -0300

    parte 109

---
+++ Work001/file075_proprioversaoLoop.py
@@ -0,0 +1,32 @@
+"""
+Propria versão de Loop
+
+
+
+"""
+
+for cada in [2, 3, 4, 5, 6]:
+    print(cada)
+
+
+for cada in 'borboleta':
+    print(cada)
+
+print('-------------------')
+
+iter([2, 3, 4, 5, 6])
+
+
+def meu_for(iteravel):
+    ite = iter(iteravel)
+    while True:
+        try:
+            print(next(ite))
+        except StopIteration:
+            break
+
+
+meu_for('jiboia')
+print('-------------')
+
+meu_for([23, 8, 6, 10])
---
+++ Work001/file076_iterator_customizado.py
@@ -0,0 +1,63 @@
+"""
+Iterator Customizado
+
+
+"""
+
+
+for n in range(5):
+    print(n)
+
+print('---------')
+
+for n in range(3, 8):
+    print(n)
+
+print('---------')
+
+
+class Contador:
+    def __init__(self, menor, maior):  # constructor
+        self.menor = menor
+        self.maior = maior
+
+
+cont = Contador(3, 8)
+print(cont)
+print(cont.menor)
+print(cont.maior)
+
+#meu_ite = iter(cont)
+
+#print(next(meu_ite)) # TyperError
+
+print('-----------------')
+
+
+class Contador2:
+    def __init__(self, menor, maior):  # constructor
+        self.menor = menor
+        self.maior = maior
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.menor < self.maior:
+            numero = self.menor
+            self.menor += 1
+            return numero
+        raise StopIteration
+
+
+cont2 = Contador2(10, 18)
+print(cont2)
+print(cont2.menor)
+print(cont2.maior)
+
+meu_ite2 = iter(cont2)
+print(next(meu_ite2))
+print(next(meu_ite2))
+print(next(meu_ite2))  # continuando até o valor máximo dá erro
+
+print('---------------')
