812780fbdd23e61cc6c07450b21d3701b28240f6
Heronnes Pereira <heronnestg@hotmail.com>
2020-08-29 22:07:06 -0300

    parte 118

---
+++ Work001/file084_forcando_tipos.py
@@ -0,0 +1,42 @@
+"""
+Forçando tipos de dados com Decoradores
+
+
+
+
+"""
+
+
+def forcar_tipo(*tipos):
+    def decorador(funcao):
+        def converter(*args, **kwargs):
+            novo_args = []
+            for (valor, tipo) in zip(args, tipos):
+                novo_args.append(tipo(valor))
+            return funcao(*novo_args, **kwargs)
+        return converter
+    return decorador
+
+
+@forcar_tipo(str, int)
+def repete_msg(msg, vezes):
+    for vez in range(vezes):
+        print(msg)
+
+
+repete_msg('python', 3)
+print('-------------')
+repete_msg('python', '3')
+
+print('-------------')
+
+
+@forcar_tipo(float, float)
+def dividir(a, b):
+    print(a/b)
+
+
+dividir('6', 3)
+dividir('7', '4')
+
+print('-------------------')
