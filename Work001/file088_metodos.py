474bfe93256a0559443ab5fc61f9dea1cc1befaa
Heronnes Pereira <heronnestg@hotmail.com>
2020-09-11 17:00:35 -0300

    parte 125

--- Work001/file088_metodos.py
+++ Work001/file088_metodos.py
@@ -14,6 +14,7 @@ objeto a partir da classe.
 obs: Métodos são escritos em letras minusculas. Se o nome for composto, o nome terá as palavras
 separadas por underline.
 
+# métodos de classe em python são conhecidos como métodos estáticos em outras linguagens
 
 """
 
@@ -116,12 +117,161 @@ usuario3 = UsuarioRef('jurandir', 'pires', 'jurandir@gmail.com','goiaba')
 
 print(usuario3._UsuarioRef__senha)  # forma nao recomendada
 
+print('----------------------')
+# Métodos de Classe
+
+
+class UsuarioRef2:
+    contador = 0
+
+    @classmethod
+    def contar_usuarios(cls):
+        print(f'Temos {cls.contador} usuários no sistema')
+
+    def __init__(self, nome, sobrenome, email, senha):
+        self.__id = UsuarioRef2.contador + 1
+        self.__nome = nome
+        self.__sobrenome = sobrenome
+        self.__email = email
+        self.__senha = criptografar.hash(senha, rounds=200000, salt_size=16) # usa-se hash(), pois encrypt() está deprecado
+        UsuarioRef2.contador = self.__id
+
+    def nome_completo(self):
+        return f'{self.__nome} {self.__sobrenome}'
+
+    def checar_senha(self, senha):
+        if criptografar.verify(senha, self.__senha):
+            return True
+        return False
+
+
+usuario4 = UsuarioRef2('Michelangelo', 'Silva', 'miko@gmail.com', '8231')
+
+UsuarioRef2.contar_usuarios()   # forma correta
+usuario4.contar_usuarios()  # possivel, mas incorreta
+# método de instancia ≠ método de classe
 
 print('----------------------')
-print('----------------------')
-print('----------------------')
-print('----------------------')
-print('----------------------')
-print('----------------------')
+
+
+class UsuarioRef3:
+    contador = 0
+
+    @classmethod
+    def contar_usuarios(cls):
+        print(f'Classe: {cls}')
+        print(f'Temos {cls.contador} usuários no sistema')
+
+    def __init__(self, nome, sobrenome, email, senha):
+        self.__id = UsuarioRef2.contador + 1
+        self.__nome = nome
+        self.__sobrenome = sobrenome
+        self.__email = email
+        self.__senha = criptografar.hash(senha, rounds=200000, salt_size=16) # usa-se hash(), pois encrypt() está deprecado
+        UsuarioRef3.contador = self.__id
+
+    def nome_completo(self):  # métodos públicos
+        return f'{self.__nome} {self.__sobrenome}'
+
+    def checar_senha(self, senha):  # métodos públicos
+        if criptografar.verify(senha, self.__senha):
+            return True
+        return False
+
+    # @classmethod  # o ideal os estáticos
+    def ver(self):
+        print('Teste')
+
+usuario5 = UsuarioRef3('Leonardo', 'da vinci', 'leo@gmail.com', '1245678')
+usuario5.contar_usuarios()
+
 print('----------------------')
 
+class UsuarioRef4:
+    contador = 0
+
+    @classmethod
+    def contar_usuarios(cls):
+        print(f'Classe: {cls}')
+        print(f'Temos {cls.contador} usuários no sistema')
+
+    def __init__(self, nome, sobrenome, email, senha):
+        self.__id = UsuarioRef2.contador + 1
+        self.__nome = nome
+        self.__sobrenome = sobrenome
+        self.__email = email
+        self.__senha = criptografar.hash(senha, rounds=200000, salt_size=16) # usa-se hash(), pois encrypt() está deprecado
+        UsuarioRef4.contador = self.__id
+        print(f'Usuario criado: {self.__gerar_usuario()}')
+
+    def nome_completo(self):  # métodos públicos
+        return f'{self.__nome} {self.__sobrenome}'
+
+    def checar_senha(self, senha):  # métodos públicos
+        if criptografar.verify(senha, self.__senha):
+            return True
+        return False
+
+    # @classmethod  # o ideal os estáticos
+    def ver(self):
+        print('Teste')
+
+    def __gerar_usuario(self):
+        return self.__email.split('@')[0]
+
+
+
+usuario6 = UsuarioRef4('Vangogh', 'van', 'goguinho@gmail.com', '87312')
+#print(usuario6.__gerar_usuario())  # dá erro
+print(usuario6._UsuarioRef4__gerar_usuario())  # acesso, de forma ruim
+
+
+print('----------------------')
+# Método Estático
+
+
+class UsuarioRef5:
+    contador = 0
+
+    @classmethod
+    def contar_usuarios(cls):
+        print(f'Classe: {cls}')
+        print(f'Temos {cls.contador} usuários no sistema')
+
+    @staticmethod
+    def definicao():
+        return 'HLX22'
+
+    def __init__(self, nome, sobrenome, email, senha):
+        self.__id = UsuarioRef2.contador + 1
+        self.__nome = nome
+        self.__sobrenome = sobrenome
+        self.__email = email
+        self.__senha = criptografar.hash(senha, rounds=200000, salt_size=16) # usa-se hash(), pois encrypt() está deprecado
+        UsuarioRef5.contador = self.__id
+        print(f'Usuario criado: {self.__gerar_usuario()}')
+
+    def nome_completo(self):  # métodos públicos
+        return f'{self.__nome} {self.__sobrenome}'
+
+    def checar_senha(self, senha):  # métodos públicos
+        if criptografar.verify(senha, self.__senha):
+            return True
+        return False
+
+    # @classmethod  # o ideal os estáticos
+    def ver(self):
+        print('Teste')
+
+    def __gerar_usuario(self):
+        return self.__email.split('@')[0]
+
+
+print(UsuarioRef5.contador)
+print(UsuarioRef5.definicao())
+
+usuario7 = UsuarioRef5('Nelson', 'cavaquinho', 'violaoecavacos@gmail.com', '91341')
+
+print(UsuarioRef5.contador)
+print(UsuarioRef5.definicao())
+print('----------------------')
