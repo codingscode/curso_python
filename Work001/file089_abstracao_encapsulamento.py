85b594b52ac1f6eb9debf380cfba1be0f473562f
Heronnes Pereira <heronnestg@hotmail.com>
2020-09-18 18:01:03 -0300

    parte 127

--- Work001/file089_abstracao_encapsulamento.py
+++ Work001/file089_abstracao_encapsulamento.py
@@ -37,10 +37,10 @@ class Conta:
     contador = 400
 
     def __init__(self, titular, saldo, limite):
-        self.numero = Conta.contador
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.numero = Conta.contador  # publico, nao boa pratica
+        self.titular = titular  # publico, nao boa pratica
+        self.saldo = saldo  # publico, nao boa pratica
+        self.limite = limite  # publico, nao boa pratica
         Conta.contador += 1
 
     def extrato(self):  # metodo de instancia
@@ -60,8 +60,133 @@ print(conta1.saldo)
 print(conta1.limite)
 print(conta1.numero)
 
+conta1.numero = 42
+conta1.titular = 'Bob'
+conta1.saldo = 9999
+conta1.limite = 88888
+
+print(conta1.__dict__)
+
+conta1.extrato()
+
+print('1---------------------------')
 
 
+class Conta2:
+
+    contador = 400
+
+    def __init__(self, titular, saldo, limite):
+        self.__numero = Conta2.contador
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+        Conta2.contador += 1
+
+    def extrato(self):  # metodo de instancia
+        print(f'Saldo de {self.__saldo} do titular {self.__titular} com limite de {self.__limite}')
+
+    def depositar(self, valor):  # metodo de instancia
+        if valor > 0:
+            self.__saldo += valor
+        else:
+            print('O valor tem que ser positivo')
+
+    def sacar(self, valor):  # metodo de instancia
+        if valor > 0:
+            if valor <= self.__saldo:
+                self.__saldo -= valor
+            else:
+                print('Saldo Insuficiente')
+        else:
+            print('O valor tem que ser positivo')
 
 
+conta2 = Conta2('TechGo', 32000, 200000)
+print(conta2.__dict__)
 
+conta2._Conta2__titular = 'Celso'
+conta2._Conta2__saldo = 40000
+
+print(conta2.__dict__)
+conta2.depositar(2300)
+print(conta2.__dict__)
+conta2.depositar(-1000)
+print(conta2.__dict__)
+
+conta2.sacar(800)
+print(conta2.__dict__)
+conta2.sacar(50000)
+print(conta2.__dict__)
+conta2.sacar(-2100)
+print(conta2.__dict__)
+
+print('2---------------------------')
+
+conta3 = Conta2('Softway', 1200, 6000)
+conta3.extrato()
+
+conta4 = Conta2('Compfire', 2300, 8000)
+conta4.extrato()
+
+# transferindo de uma conta para outra (forma não adequada)
+valor = 100
+
+conta3.sacar(valor)
+conta4.depositar(valor)
+
+conta3.extrato()
+conta4.extrato()
+
+print('3---------------------------')
+
+
+class Conta3:
+
+    contador = 400
+
+    def __init__(self, titular, saldo, limite):
+        self.__numero = Conta3.contador
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+        Conta3.contador += 1
+
+    def extrato(self):  # metodo de instancia
+        print(f'Saldo de {self.__saldo} do titular {self.__titular} com limite de {self.__limite}')
+
+    def depositar(self, valor):  # metodo de instancia
+        if valor > 0:
+            self.__saldo += valor
+        else:
+            print('O valor tem que ser positivo')
+
+    def sacar(self, valor):  # metodo de instancia
+        if valor > 0:
+            if valor <= self.__saldo:
+                self.__saldo -= valor
+            else:
+                print('Saldo Insuficiente')
+        else:
+            print('O valor tem que ser positivo')
+
+    def transferir(self, valor, conta_destino):
+        # 1 remover o valor da conta de origem
+        self.__saldo -= valor + 10  #  taxa de transferencia
+        # 2 adicionar o valor na conta de destino
+        conta_destino.__saldo += valor
+
+
+# Transferindo de forma adequada
+conta5 = Conta3('ChinaImportados', 5500, 18000)
+conta5.extrato()
+
+conta6 = Conta3('ItalyGourmet', 12300, 37000)
+conta6.extrato()
+
+conta5.transferir(100, conta6)
+
+conta5.extrato()
+conta6.extrato()
+
+print('4---------------------------')
