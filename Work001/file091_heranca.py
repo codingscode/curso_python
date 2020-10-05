d8ed04002a9e08ec38e8b33e9779a1d8ac15060d
Heronnes Pereira <heronnestg@hotmail.com>
2020-10-04 22:10:22 -0300

    parte 138

---
+++ Work001/file091_heranca.py
@@ -0,0 +1,95 @@
+"""
+Continuação POO -> Herança (Inheritance)
+
+
+A ideia de herança é a de reaproveitar código. Também extender nossas classes.
+Obs: Com a herança, a partir de uma classe existente, nós extendemos outra classe que passa a herdar
+ atributos e métodos da classe herdada.
+
+
+Pergunta: Existe alguma entidade genérica o suficiente para encapsular os atributos e métodos comuns
+ a outras entidades ?
+
+OBS: Quando uma classe herda de outra classe ela herda todos os atributos e métodos da classe herdada.
+
+Quando uma classe herda de outra classe, a classe herdada é conhecida por:
+- Super Classe
+- Classe Mãe
+- Classe Pai
+- Classe Genérica
+
+Quando uma classe herda de outra classe, ela é chamada de:
+- Sub Classe
+- Classe Filha
+- Classe Específica
+
+"""
+
+
+class Cliente:
+
+    def __init__(self, nome, sobrenome, cpf, renda):
+        self.__nome = nome
+        self.__sobrenome = sobrenome
+        self.__cpf = cpf
+        self.__renda = renda
+
+    def nome_completo(self):
+        return f'{self.__nome} {self.__sobrenome}'
+
+
+class Funcionario:
+
+    def __init__(self, nome, sobrenome, cpf, matricula):
+        self.__nome = nome
+        self.__sobrenome = sobrenome
+        self.__cpf = cpf
+        self.__matricula = matricula
+
+    def nome_completo(self):
+        return f'{self.__nome} {self.__sobrenome}'
+
+
+cliente1 = Cliente('Simon', 'Nooks', 57942755538, 1200)
+funcionario1 = Funcionario('Bob', 'mile', 25841953857, 374572645)
+
+print(cliente1.nome_completo())
+print(funcionario1.nome_completo())
+
+print('1---------------------')
+
+
+class Pessoa:
+
+    def __init__(self, nome, sobrenome, cpf):
+        self.__nome = nome
+        self.__sobrenome = sobrenome
+        self.__cpf = cpf
+
+    def nome_completo(self):
+        return f'{self.__nome} {self.__sobrenome}'
+
+
+class ClienteRef1(Pessoa):  # ClienteRef1 herda de Pessoa
+
+    def __init__(self, nome, sobrenome, cpf, renda):
+        super().__init__(nome, sobrenome, cpf)
+        self.__renda = renda
+
+
+class FuncionarioRef1(Pessoa):  #  FuncionarioRef1 herda de Pessoa
+
+    def __init__(self, nome, sobrenome, cpf, matricula):
+        super().__init__(nome, sobrenome, cpf)
+        self.__matricula = matricula
+
+
+cliente2 = ClienteRef1('Clarence', 'Silver', 57942755538, 1200)
+funcionario2 = FuncionarioRef1('Mike', 'Stone', 25841953857, 374572645)
+
+print(cliente2.nome_completo())
+print(funcionario2.nome_completo())
+
+print('2---------------------')
+print('3---------------------')
+print('4---------------------')
