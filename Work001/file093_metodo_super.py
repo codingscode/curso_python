6f3f0275a3235763e6c1b7227497739750ae5ef2
Heronnes Pereira <heronnestg@hotmail.com>
2020-10-11 20:58:12 -0300

    parte 142

---
+++ Work001/file093_metodo_super.py
@@ -0,0 +1,30 @@
+"""
+POO - Metodo super
+
+O metodo super() se refere a super classe.
+
+
+"""
+
+
+class Animal:
+
+    def __init__(self, nome, especie):
+        self.__nome = nome
+        self.__especie = especie
+
+    def faz_som(self, som):
+        print(f'animal {self.__nome} emite: {som}')
+
+
+class Gato(Animal):
+    def __init__(self, nome, especie, raca):
+        super().__init__(nome, especie)  #  ou Animal.__init__(self, nome, especie)
+        self.__raca = raca
+        super().faz_som('miau miau')
+
+
+chiquinho = Gato('chiquinho', 'felino', 'siames')
+chiquinho.faz_som('miau')
+
+print('---------------------')
