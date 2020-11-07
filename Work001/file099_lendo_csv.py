d0f43a1bf5b40593a1ea3a82df374b9c20f7347c
Heronnes Pereira <heronnestg@hotmail.com>
2020-11-06 21:13:59 -0300

    parte 158

--- Work001/file099_lendo_csv.py
+++ Work001/file099_lendo_csv.py
@@ -8,11 +8,55 @@ ex:
 'a', 'b', 'c', 'd', 'e'
 obs: o separador também pode ser ';', espaço
 
+http://dados.gov.br/dataset
+
+A linguagem Python possui duas formas diferentes para ler dados em arquivos CSV:
+  - reader -> Permite que iteremos sobre as linhas do arquivo CSV como listas.
+  - DictReader -> Permite que iteremos sobre as linhas do arquivo CSV como OrderedDicts.
+
+
 """
 
+# Possível de se trabalhar, mas não é o ideal (trabalhoso)
+with open('file099_lutadores.csv', encoding='utf-8') as arquivo1:
+    dados = arquivo1.read()
+    #print(type(dados))
+    dados = dados.split(',')[2:]  # a partir do 2
+    print(dados)
 
+print('1---------------------')
+# Reader
 
+from csv import reader
 
+with open('file099_lutadores.csv', encoding='utf-8') as arquivo2:
+    leitor_csv = reader(arquivo2)
+    print(type(leitor_csv))
+    next(leitor_csv)  # pula o cabeçalho
+    #print(leitor_csv)
+    for linha in leitor_csv:
+        # Cada linha é uma lista
+        print(f'{linha[0]} nasceu em {linha[1]} e mede {linha[2]} centimetros')
 
+print('2---------------------')
+# DictReader
 
+from csv import DictReader
 
+with open('file099_lutadores.csv', encoding='utf-8') as arquivo3:
+   leitor_csv = DictReader(arquivo3)
+
+   for linha in leitor_csv:
+       # Cada linha é um OrderedDict
+       print(f"{linha['Nome']} nasceu no(a)(s) {linha['País']} e mede {linha['Altura']}")
+
+print('3---------------------')
+
+with open('file099_lutadores.csv', encoding='utf-8') as arquivo4:
+   leitor_csv = DictReader(arquivo4, delimiter=',')  # separador -> ','
+
+   for linha in leitor_csv:
+       # Cada linha é um OrderedDict
+       print(f"{linha['Nome']} nasceu no(a)(s) {linha['País']} e mede {linha['Altura']}")
+
+print('4---------------------')
