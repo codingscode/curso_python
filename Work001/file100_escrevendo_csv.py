ce2d624195b2f3ce6746bf9e13be595bf763bd74
Heronnes Pereira <heronnestg@hotmail.com>
2020-11-08 19:21:53 -0300

    parte 160

--- Work001/file100_escrevendo_csv.py
+++ Work001/file100_escrevendo_csv.py
@@ -10,7 +10,7 @@ writerow() -> Escreve uma linha
 # writer() -> Gera um objeto para que possamos escrever em um arquivo CSV. utilizamos o metodo writerow para escrever cada linha. Este metodo recebe uma lista.
 from csv import writer
 
-with open('file100_criandoarquivo.csv', 'w', encoding='utf-8') as arquivo:  # 'a' -> adiciona ao conteudo presente
+with open('file100_criandoarquivo.csv', 'a', encoding='utf-8', newline='') as arquivo:  # 'a' -> adiciona ao conteudo presente
     escritor_csv = writer(arquivo)
     filme = None
     escritor_csv.writerow(['Titulo', 'Genero', 'Duração'])
@@ -21,8 +21,24 @@ with open('file100_criandoarquivo.csv', 'w', encoding='utf-8') as arquivo:  # 'a
             duracao = input('Informe a duração em minutos: ')
             escritor_csv.writerow([filme, genero, duracao])
 
+print('1-----------------')
+# DictWriter
+# obs: as chaves do dicionario devem ser as mesmas utilizadas como cabeçalho.
+
+from csv import DictWriter
+
+with open('file100_criandoarquivo2.csv', 'w', encoding='utf-8', newline='') as arquivo:  # experimentar 'a' no lugar de 'w'
+    cabecalho = ['Titulo', 'Genero', 'Duracao']
+    escritor_csv = DictWriter(arquivo, fieldnames=cabecalho)
+    escritor_csv.writeheader()
+    filme = None
+    while filme != 'sair':
+        filme = input('Informe o nome do filme: ')
+        if filme != 'sair':
+            genero = input('Informe o gênero: ')
+            duracao = input('Informe a duração (minutos): ')
+            escritor_csv.writerow({'Titulo': filme, 'Genero': genero, 'Duracao': duracao})
 
 
-print('-----------------')
-print('-----------------')
-print('-----------------')
+print('2-----------------')
+print('3-----------------')
