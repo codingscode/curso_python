ad8e4cbb1f32bc30f8f9eaec0403221a67e0a0b1
Heronnes Pereira <heronnestg@hotmail.com>
2020-11-16 16:47:39 -0300

    parte 166

--- Work001/file103_manipulando_data_hora.py
+++ Work001/file103_manipulando_data_hora.py
@@ -24,9 +24,36 @@ print(inicio)
 inicio = inicio.replace(hour=16, minute=0, second=0, microsecond=0)
 print(inicio)
 
-
 print('1-----------------------')
+
+evento = datetime.datetime(2019, 1, 1, 0)
+print(evento)
+print(type(evento))
+print(type('03/02/2020'))
+
 print('2-----------------------')
+nascimento = input('Informe sua data de nascimento (dd/mm/yyyy): ')
+nascimento = nascimento.split('/')
+print(nascimento)
+print(type(nascimento))
+nascimento = datetime.datetime(int(nascimento[2]), int(nascimento[1]), int(nascimento[0]))
+#nascimento = datetime.datetime(year=int(nascimento[2]), month=int(nascimento[1]), day=int(nascimento[0]))
+print(nascimento)
+print(type(nascimento))
+
 print('3-----------------------')
+# Acessa individual dos elementos de data e hora
+evento2 = datetime.datetime.now()
+print(evento2)
+print(f'ano : {evento2.year}')
+print(f'mês : {evento2.month}')
+print(f'dia : {evento2.day}')
+print(f'hora : {evento2.hour}')
+print(f'minuto : {evento2.minute}')
+print(f'segundo : {evento2.second}')
+print(f'microsegundo : {evento2.microsecond}')
+
+print(dir(evento2))
+
 print('4-----------------------')
 print('5-----------------------')
