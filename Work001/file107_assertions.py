165631f88457b2365a8182881e259c0052193d03
Heronnes Pereira <heronnestg@hotmail.com>
2020-11-24 20:11:20 -0300

    parte 174

--- Work001/file107_assertions.py
+++ Work001/file107_assertions.py
@@ -13,15 +13,39 @@ personalizado.
 # Obs: A palavra 'assert' pode ser utilizada em qualquer função ou código nosso... não precisa ser
 exclusivamente nos testes.
 
+obs: cuidado ao usar 'assert'
+Se um programa Python for executado com o parametro -O, nenhum assertion será validado. Ou seja,
+todas as suas validações já eram.
+
 
 """
 
 """
 no terminal assert 4 == 4
+            assert 4 == 1
+            assert 4 == 1, '4 não é 1'
 """
 
 
+def soma_numeros_positivos(a, b):
+    assert a > 0 and b > 0, 'Ambos números precisam ser positivos'
+    return a + b
 
 
+retorno1 = soma_numeros_positivos(2, 4)
+print(retorno1)
+
+#retorno2 = soma_numeros_positivos(-3, 8)  # dá erro
+#print(retorno2)
 
 
+def comer_fast_food(comida):
+    assert comida in ['pizza', 'sorvete', 'doces', 'batata frita', 'cachorro-quente'], 'A comida precisa ser fast food'
+    return f'Eu estou comendo {comida}'
+
+
+print(comer_fast_food('pizza'))
+#print(comer_fast_food('laranja'))  # dá erro
+
+print('1------------------')
+print('2------------------')
