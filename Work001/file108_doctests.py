6bf4d5ed3abef2367cb0097d60af0c16e4979d59
Heronnes Pereira <heronnestg@hotmail.com>
2020-11-27 22:22:57 -0300

    parte 176

--- Work001/file108_doctests.py
+++ Work001/file108_doctests.py
@@ -46,4 +46,44 @@ no terminal da pasta do arquivo: python -m doctest -v file108_doctests.py
 """
 
 print('1-----------------')
+# aplicando TDD
+
+
+def duplicar(valores):
+    """ duplicar os valores em uma lista
+    >>> duplicar([1, 2, 3, 4])
+    [2, 4, 6, 8]
+
+    >>> duplicar([])
+    []
+
+    >>> duplicar(['a', 'b', 'c'])
+
+    ['aa', 'bb', 'cc']
+    >>> duplicar([True, None])
+    Traceback (most recent call last):
+      ...
+    TypeError: unsupported operand type(s) for *: 'int' and 'NoneType'
+    """
+    # pass antes
+    #return []  # depois1
+    return [2*elemento for elemento in valores]  # depois2
+
+"""
+fazer exemplo acima no terminal da pasta deste arquivo
+"""
+
 print('2-----------------')
+# Erro inesperado
+
+
+def fala_oi():
+    """Fala oi
+    >>> fala_oi()
+    "oi"
+    """
+    return "oi"
+
+
+print('3-----------------')
+print('4-----------------')
