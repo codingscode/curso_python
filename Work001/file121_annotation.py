6c099f565df770ab720bc80de5a0776eb0f084ca
Heronnes Pereira <heronnestg@hotmail.com>
2020-12-27 12:10:03 -0300

    parte 190

---
+++ Work001/file121_annotation.py
@@ -0,0 +1,80 @@
+"""
+
+"""
+
+
+def cabecalho(texto: str, alinhamento: bool = True) -> str:
+    if alinhamento:
+        return f"{texto.title()}\n{'*'*len(texto)}"
+    else:
+        return f"{texto.title()} ".center(50, '#')
+
+
+print(cabecalho('codando em python'))
+print(cabecalho('usando python em django', alinhamento=False))
+print(cabecalho('code em python', alinhamento='geek'))  # agora checa
+
+print('1-----------------')
+"""
+terminal da pasta do arquivo: mypy file120_mypy.py
+
+file120_mypy.py:18: error: Argument "alinhamento" to "cabecalho" has incompatible type "str"; expected "bool"
+Found 1 error in 1 file (checked 1 source file)
+
+"""
+
+# Correto
+texto: str
+
+# Incorreto
+texto:str
+texto : str
+
+""""""
+
+import math
+
+
+def circunferencia(raio: float) -> float:
+    return 2*math.pi*raio
+
+
+print(circunferencia.__annotations__)
+
+print('2--------------')
+
+nome: str = 'codando em python'
+peso: float = 67.8
+
+# não são duas declarações
+ativo: bool
+ativo = True   # preferivel ativo: bool = True
+
+print(nome)
+print(peso)
+print(ativo)
+print('**', __annotations__)
+
+print('3-----------------')
+
+
+class Pessoa:
+
+    def __init__(self, nome2: str, idade2: int, peso2: float) -> None:
+        self.__nome2: str = nome2
+        self.__idade2: int = idade2
+        self.__peso2: float = peso2
+
+    def andar(self) -> str:  # def andar(self: Pessoa) -> str:  daria erro
+        return f'{self.__nome} está andando.'
+
+
+p = Pessoa(nome2='Aladin', idade2=2, peso2=4)
+
+print(p.__dict__)
+#print(p.__annotations__)  # dá erro
+print(p.andar.__annotations__)
+print(p.__init__.__annotations__)
+
+
+print('4-----------------')
