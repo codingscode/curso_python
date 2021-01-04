22e8e629b4fd44698c67412ade6302a1664ddedf
Heronnes Pereira <heronnestg@hotmail.com>
2021-01-04 09:27:29 -0300

    parte 198

--- Work001/file127_tipos_dados_mais_precisos.py
+++ Work001/file127_tipos_dados_mais_precisos.py
@@ -2,7 +2,15 @@
 Tipos de Dados mais precisos
 
 int, str, float, List, Set, Dict, etc
--m
+
+
+- Literal type
+- Union
+- Final
+- Typed dictionaries
+- Protocols
+
+
 """
 
 
@@ -18,9 +26,65 @@ print(dobro('pula-pula '))
 executar no terminal da pasta do arquivo: mypy file127_tipos_dados_mais_precisos.py
 """
 
-
 print('1---------------------')
+# Literal type
+
+from typing import Literal, Union
+
+
+def pegar_status(usuario: str) -> Literal['conectado', 'desconectado']:
+    pass
+
+
+def calcular_v1(operacao: str, num1: int, num2: int) -> None:
+    if operacao == '+':
+        print(f'{num1} + {num2} = {num1 + num2}')
+    elif operacao == '-':
+        print(f'{num1} - {num2} = {num1 - num2}')
+    else:
+        raise ValueError(f'Operação inválida {operacao!r}')
+
+
+calcular_v1('+', 4, 5)
+calcular_v1('-', 10, 7)
+calcular_v1('/', 10, 5)  # chama o erro, mas o mypy nao acha
+"""
+usar o mypy
+"""
+
 print('2---------------------')
+
+
+def calcular_v2(operacao: Literal['+', '-'], num1: int, num2: int) -> None:
+    if operacao == '+':
+        print(f'{num1} + {num2} = {num1 + num2}')
+    elif operacao == '-':
+        print(f'{num1} - {num2} = {num1 - num2}')
+    else:
+        raise ValueError(f'Operação inválida {operacao!r}')
+
+
+calcular_v2('+', 4, 5)
+calcular_v2('-', 10, 7)
+calcular_v2('/', 10, 5)  # chama o erro, mas o mypy acha
+
+
 print('3---------------------')
+# Union
+
+
+def somar(num1: int, num2: int) -> Union[str, int]:
+    resultado: int = num1 + num2
+    if resultado > 50:
+        return f'O valor {resultado} é muito grande.'
+    else:
+        return resultado
+
+
+
+
+
+
 print('4---------------------')
 print('5---------------------')
+print('6---------------------')
