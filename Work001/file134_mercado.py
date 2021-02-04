4a93dbbd0e4eacb5ccf56e955fa0688929c06596
Heronnes Pereira <heronnestg@hotmail.com>
2021-02-04 20:44:38 -0300

    parte 209

--- Work001/file134_mercado.py
+++ Work001/file134_mercado.py
@@ -82,11 +82,64 @@ def listar_produtos() -> None:
 
 
 def comprar_produto() -> None:
-    pass
+    if len(produtos) > 0:
+        print('Informe o código do produto que deseja adicionar ao carrinho: ')
+        print('--------------------------------------------------------------')
+        print('=============== Produtos Disponíveis =========================')
+        for produto in produtos:
+            print(produto)
+            print('----------------------------------------------------------')
+            sleep(1)
+        codigo: int = int(input())
+        produto: Produto = pega_produto_por_codigo(codigo)
+
+        if produto:
+            if len(carrinho) > 0:
+                tem_no_carrinho: bool = False
+                for item in carrinho:
+                    quant: int = item.get(produto)
+                    if quant:
+                        item[produto] = quant + 1
+                        print(f'O produto {produto.nome} agora possui {quant + 1} unidades no carrinho.')
+                        tem_no_carrinho = True
+                        sleep(2)
+                        menu()
+                if not tem_no_carrinho:
+                    prod = {produto: 1}
+                    carrinho.append(prod)
+                    print(f'O produto {produto.nome} foi adicionado ao carrinho.')
+                    sleep(2)
+                    menu()
+            else:
+                item = {produto: 1}
+                carrinho.append(item)
+                print(f'O produto {produto.nome} foi adicionado ao carrinho.')
+                sleep(2)
+                menu()
+        else:
+            print(f'O produto com código {codigo} não foi encontrado.')
+            sleep(2)
+            menu()
+    else:
+        print('Ainda não existem produtos para vender.')
+    sleep(2)
+    menu()
 
 
 def visualizar_carrinho() -> None:
-    pass
+    if len(carrinho) > 0:
+        print('Produtos no carrinho: ')
+
+        for item in carrinho:
+            for dados in item.items():
+                print(dados[0])
+                print(f'Quantidade: {dados[1]}')
+                print('------------------------')
+                sleep(1)
+    else:
+        print('Ainda não existem produtos no carrinho.')
+    sleep(2)
+    menu()
 
 
 def fechar_pedido() -> None:
