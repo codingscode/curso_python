1d180bc52208c8f00df37b61b92bc216909354d1
Heronnes Pereira <heronnestg@hotmail.com>
2021-02-10 19:50:24 -0300

    parte 212

---
+++ Work001/file135_banco.py
@@ -0,0 +1,45 @@
+from typing import List
+from time import sleep
+
+
+from file135_models.cliente import Cliente
+from file135_models.conta import Conta
+
+
+contas: List[Conta] = []
+
+
+def main() -> None:
+    menu()
+
+
+def menu() -> None:
+    pass
+
+
+def criar_conta() -> None:
+    pass
+
+
+def efetuar_saque() -> None:
+    pass
+
+
+def efetuar_deposito() -> None:
+    pass
+
+
+def efetuar_transferencia() -> None:
+    pass
+
+
+def listar_contas() -> None:
+    pass
+
+
+def buscar_conta_por_numero(numero: int) -> None:
+    pass
+
+
+if __name__ == '__main__':
+    main()
