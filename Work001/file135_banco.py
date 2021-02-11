7af4fea9448d623164cd5667fcbb196029942a87
Heronnes Pereira <heronnestg@hotmail.com>
2021-02-11 11:08:27 -0300

    parte 213

--- Work001/file135_banco.py
+++ Work001/file135_banco.py
@@ -14,15 +14,77 @@ def main() -> None:
 
 
 def menu() -> None:
-    pass
+    print('=========================================')
+    print('================== ATM ==================')
+    print('================ Geek Bank ==============')
+    print('=========================================')
+
+    print('Selecione uma opção no menu: ')
+    print('1 - Criar conta')
+    print('2 - Efetuar saque')
+    print('3 - Efetuar depósito')
+    print('4 - Efetuar transferência')
+    print('5 - Listar contas')
+    print('6 - Sair do sistema')
+
+    opcao: int = int(input())
+
+    if opcao == 1:
+        criar_conta()
+    elif opcao == 2:
+        efetuar_saque()
+    elif opcao == 3:
+        efetuar_deposito()
+    elif opcao == 4:
+        efetuar_transferencia()
+    elif opcao == 5:
+        listar_contas()
+    elif opcao == 6:
+        print('Volte sempre')
+        sleep()
+        exit(0)
+    else:
+        print('Opção inválida')
+        sleep(2)
+        menu()
 
 
 def criar_conta() -> None:
-    pass
+    print('Informe os dados do cliente: ')
+
+    nome: str = input('Nome do cliente: ')
+    email: str = input('E-mail do cliente: ')
+    cpf: str = input('CPF do cliente: ')
+    data_nascimento: str = input('Data de nascimento do cliente: ')
+
+    cliente: Cliente = Cliente(nome, email, cpf, data_nascimento)
+    conta: Conta = Conta(cliente)
+
+    contas.append(conta)
+
+    print('Conta criada com sucesso.')
+    print('Dados da conta: ')
+    print('----------------------------------')
+    print(conta)
+    sleep(2)
+    menu()
 
 
 def efetuar_saque() -> None:
-    pass
+    if len(contas) > 0:
+        numero: int = int('Informe o número da sua conta: ')
+
+        conta: Conta = buscar_conta_por_numero(numero)
+
+        if conta:
+            valor: float = float(input('Informe o valor do saque: '))
+            conta.sacar(valor)
+        else:
+            print('Não foi encontrada a conta com número {numero}')
+    else:
+        print('Ainda não existem contas cadastradas.')
+    sleep(2)
+    menu()
 
 
 def efetuar_deposito() -> None:
