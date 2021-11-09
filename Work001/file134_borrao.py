d1dff530fcefcf81d82b6c6cbdab0e8312747519
codingscode <heronnestg@hotmail.com>
2021-11-08 23:11:27 -0300

    rev-109

---
+++ Work001/file134_borrao.py
@@ -0,0 +1,13 @@
+
+print('algo...')
+
+
+lista = [
+    {'nome': 'paulo', 'idade': 21, 'codigo': 1},
+    {'nome': 'maria', 'idade': 17, 'codigo': 2},
+    {'nome': 'ze', 'idade': 25, 'codigo': 3},
+    {'nome': 'vicente', 'idade': 10, 'codigo': 4},
+]
+
+print(lista)
+
